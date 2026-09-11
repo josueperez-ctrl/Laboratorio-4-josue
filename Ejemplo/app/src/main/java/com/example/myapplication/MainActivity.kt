a8fa60e1faffcee5bf904acfd3bca22a41419f8d
josueperez-ctrl <josue.perez@tecsup.edu.pe>
2026-09-11 12:35:34 -0500

    Commit 2 swich

--- Ejemplo/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Ejemplo/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,11 +50,18 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
-        Card(
-            modifier = Modifier.padding(16.dp),
-        ) {
-            Text(text = "Tarjeta de ejemplo", modifier = Modifier.padding(16.dp))
+        Column(modifier = Modifier.padding(16.dp)) {
+            Greeting("Android")
+            Card(
+                modifier = Modifier.padding(vertical = 8.dp),
+            ) {
+                Text(text = "Tarjeta de ejemplo", modifier = Modifier.padding(16.dp))
+            }
+            var isChecked by remember { mutableStateOf(false) }
+            Switch(
+                checked = isChecked,
+                onCheckedChange = { isChecked = it },
+            )
         }
     }
 }
