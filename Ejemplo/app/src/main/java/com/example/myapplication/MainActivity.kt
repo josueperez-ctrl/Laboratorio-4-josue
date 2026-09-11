c66fc44c38bc94ef36dc8bc40b67fc1c5942b528
josueperez-ctrl <josue.perez@tecsup.edu.pe>
2026-09-11 12:26:07 -0500

    Commit 1

--- Ejemplo/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Ejemplo/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
                     )
                 }
             }
@@ -43,5 +45,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     MyApplicationTheme {
         Greeting("Android")
+        Card(
+            modifier = Modifier.padding(16.dp),
+        ) {
+            Text(text = "Tarjeta de ejemplo", modifier = Modifier.padding(16.dp))
+        }
     }
 }
